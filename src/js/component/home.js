9c9774fb46a66f64398e886a149e6345d8860e9f
Yadira11 <chavarriayadira85@gmail.com>
2021-02-10 21:13:44 +0000

    finish-tic-tac

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,67 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+export class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			UserInput: "X",
+			cell1: "",
+			cell2: "",
+			cell3: "",
+			cell4: "",
+			cell5: "",
+			cell6: "",
+			cell7: "",
+			cell8: "",
+			cell9: ""
+		};
+	}
+	Jugar = cellId => {
+		const newState = {};
+		newState[cellId] = this.state.UserInput;
+		if (this.state[cellId] === "") {
+			this.setState(newState);
+			this.setState({
+				UserInput: this.state.UserInput === "X" ? "O" : "X"
+			});
+		}
+	};
+	render() {
+		return (
+			<div id="container" className="text-center mt-5">
+				<div className="game-row">
+					<div className="cell" onClick={() => this.Jugar("cell1")}>
+						{this.state.cell1}
+					</div>
+					<div className="cell" onClick={() => this.Jugar("cell2")}>
+						{this.state.cell2}
+					</div>
+					<div className="cell" onClick={() => this.Jugar("cell3")}>
+						{this.state.cell3}
+					</div>
+				</div>
+				<div className="game-row">
+					<div className="cell" onClick={() => this.Jugar("cell4")}>
+						{this.state.cell4}
+					</div>
+					<div className="cell" onClick={() => this.Jugar("cell5")}>
+						{this.state.cell5}
+					</div>
+					<div className="cell" onClick={() => this.Jugar("cell6")}>
+						{this.state.cell6}
+					</div>
+				</div>
+				<div className="game-row">
+					<div className="cell" onClick={() => this.Jugar("cell7")}>
+						{this.state.cell7}
+					</div>
+					<div className="cell" onClick={() => this.Jugar("cell8")}>
+						{this.state.cell8}
+					</div>
+					<div className="cell" onClick={() => this.Jugar("cell9")}>
+						{this.state.cell9}
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
